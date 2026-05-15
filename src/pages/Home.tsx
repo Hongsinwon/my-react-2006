1bb6460e7502f33fdad0f0cb55a118bc83b5589f
Hongsinwon <ghdtlsdnjs12@naver.com>
2026-05-15 17:28:23 +0900

    docs : 네이버 검색 API 타입 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,25 @@
 import { useEffect } from 'react';
 import { getSearch } from '../api/search';
 
+interface SearchData {
+  bloggerlink: string;
+  bloggername: string;
+  description: string;
+  link: string;
+  postdate: string;
+  title: string;
+}
+
+interface SearchResponse {
+  items: SearchData[] | null;
+}
+
 export default function Home() {
   useEffect(() => {
     const fetchSearchData = async () => {
       try {
         const data = await getSearch('react');
-        console.log((data as any).items);
+        console.log((data as SearchResponse).items);
       } catch (err) {
         console.log('데이터 가져오기 실패----', err);
       }
